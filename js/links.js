0275f90bf66790cfa020f559d5ad6d4fd550dd67
bhartik021 <bhartik021@gmail.com>
2022-09-06 10:33:44 +0530

    updated links in links.js

---
+++ js/links.js
@@ -0,0 +1,27 @@
+export const links = [
+    {
+      name: "Twitter",
+      link: "https://twitter.com/bhartik021",
+      image: "./assets/twitter.png",
+    },
+    {
+      name: "Linkedin",
+      link: "https://www.linkedin.com/bhartik021/",
+      image: "./assets/linkedin.png",
+    },
+    {
+      name: "Instagram",
+      link: "https://www.instagram.com/bhartik021/",
+      image: "./assets/instagram.png",
+    },
+    {
+      name: "E-mail",
+      link: "mailto:bhartik021@gmail.com",
+      image: "./assets/gmail.png",
+    },
+    {
+      name: "Github",
+      link: "https://github.com/bhartik021",
+      image: "./assets/github.png",
+    },
+  ];
